7dc701a5336646e666f3a105c829037302d0ca05
Sergey Vedmedev <vedmedev@trueconf.ru>
2019-03-31 11:36:14 +0300

    test

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,6 +1,24 @@
 <template>
-  <div>
-   
+  <div class="calculator">
+    <div class="display">0.1346346</div>
+    <div>C</div>
+    <div>+/-</div>
+    <div>%</div>
+    <div>7</div>
+    <div>8</div>
+    <div>9</div>
+    <div>x</div>
+    <div>4</div>
+    <div>5</div>
+    <div>6</div>
+    <div>-</div>
+    <div>1</div>
+    <div>2</div>
+    <div>3</div>
+    <div class="zero">0</div>
+    <div>.</div>
+    <div>=</div>
+
   </div>
 </template>
 
@@ -11,5 +29,19 @@ export default {
 </script>
 
 <style scoped>
+.calculator {
+  font-size: 40px;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+}
 
+.display{
+  grid-column: 1 / 5;
+  background-color:red;
+}
+
+.zero {
+  grid-column: 1 / 3;
+}
 </style>
