650f19cc8bdececa214ba2ad292421c274c1e142
Sergey Vedmedev <vedmedev@trueconf.ru>
2019-04-13 12:24:43 +0300

    almost done

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -27,7 +27,10 @@
 export default {
   data() {
     return {
+      previous: null,
       current: '',
+      operator: null,
+      operatorClicked: false,
     }
   },
   methods: {
@@ -42,9 +45,43 @@ export default {
       this.current = `${parseFloat(this.current)/100}`
     },
     append(number) {
+      if (this.operatorClicked) {
+        this.current = ''
+        this.operatorClicked = false
+      }
       this.current = `${this.current}${number}`
     },
-    dot () {}
+    dot () {
+      if (this.current.indexOf('.') === -1) {
+        this.append('.')
+      }
+    },
+    setPrevious() {
+      this.previous = this.current
+      this.operatorClicked = true
+    },
+    divide() {
+      this.operator = (a,b) => a / b
+      this.setPrevious()
+    },
+    times() {
+      this.operator = (a,b) => a * b
+      this.setPrevious()
+    },
+    minus() {
+      this.operator = (a,b) => a - b
+      this.setPrevious()
+    },
+    add() {
+      this.operator = (a,b) => a + b
+      this.setPrevious()
+    },
+    equal() {
+      this.current = `${this.operator(
+        parseFloat(this.current),
+        parseFloat(this.previous)
+      )}`
+    }
   },
 }
 </script>
